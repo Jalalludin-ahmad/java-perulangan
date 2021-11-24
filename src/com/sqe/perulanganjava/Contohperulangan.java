ef4aae948dc027ed1f9710a7401ee3da6a82c98a
jalalgalang@gmail.com <contoh@email.com>
2021-11-24 11:56:00 +0700

    first commit

---
+++ src/com/sqe/perulanganjava/Contohperulangan.java
@@ -0,0 +1,12 @@
+package com.sqe.perulanganjava;
+
+public class Contohperulangan {
+	public static void main(String[] args) {
+		//aplikasi raraban
+		int N= 8;
+		for(int i=1;i<=10;i++) {
+			System.out.printf("%d x %d = %d%n", N, i, N*i);
+		}
+	}
+
+}
